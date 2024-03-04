0c46acbf636acfbfaded522ffbb8dfd4422ca48e
Craig Clayton <craig__clayton@outlook.com>
2024-03-04 17:27:39 +0000

    add tailwind

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -10,3 +10,8 @@ export const getProducts = async () => {
   const { data } = await api.get(`/products`);
   return data;
 };
+
+export const getProductById = async (product_id) => {
+  const { data } = await api.get(`/products/${product_id}`);
+  return data;
+};
